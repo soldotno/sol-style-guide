db02ace1cf771d7aee64e5f0815601f6f4c357ef
sisve <sinsvend@gmail.com>
2015-10-20 12:42:23 +0200

    Wip: adding express

--- README.md
+++ README.md
@@ -2,15 +2,9 @@ SOL's style guide
 =================
 Providing a consistent visual profile across projects.
 
-http://soldotno.github.io/sol-style-guide
+http://sol-style-guide.herokuapp.com/sol-style-guide
 
 Developing and launching
 ------------------------
 
-When using this repository as a dependency, symlink it locally with `npm link`.
-
-After a pull request has been approved to be merged into the gh-pages branch, bump the version number and publish it to nexus.sol.no:
-
-    npm version major|minor|patch
-    npm publish
-    git push
+Master branch is deployed to sol-style-guide.herokuapp.com
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,15 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cssmin = require('gulp-cssmin');
 var NopStream = require('../util/no-op-stream');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
+
 
 var production = process.env.NODE_ENV === 'production';
 
 gulp.task('sass', function() {
     gulp.src('./scss/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer({
+          browsers: ["> 2% in NO" ]
+        }))
         .pipe(production ? cssmin() : (new NopStream()))
         .pipe(gulp.dest('./css'))
-        .pipe(reload({stream: true}));
 });
